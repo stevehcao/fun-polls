0b5c76daa0d441bc45beb11b62e76fab6f2fa970
Steve Cao <steve.cao7@gmail.com>
2019-02-08 10:56:25 -0800

    able to grab data and build question form from database

--- src/components/Poll.vue
+++ src/components/Poll.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="poll">
     <h1>Poll goes here</h1>
-    <div class="question">
+    <div v-if="polls[0]" class="question">
       <h3>{{ polls[0].question }}</h3>
       <form @submit.prevent="countVote(voted)">
         <!-- radio buttons or checkbox for each question choice have to loop through the ans array -->
@@ -15,6 +15,7 @@
         <button>Rock the Vote!</button>
       </form>
     </div>
+    <div v-else class="loading">Loading poll question...</div>
   </div>
 </template>
 
@@ -28,17 +29,7 @@ export default {
       // polls are an array of objects that contains poll questions and answers
       // dummy data for now until we hook up database
       // possibly grab all data and then shuffle using Fisher-Yates
-      polls: [
-        {
-          question: "What is your favorite 2018 video game?",
-          answers: [
-            { choice: "Spider-Man", votes: 0, id: 1 },
-            { choice: "Super Smash Bros. Ultimate", votes: 0, id: 2 },
-            { choice: "Red Dead Redemption 2", votes: 0, id: 3 },
-            { choice: "God of War", votes: 0, id: 4 }
-          ]
-        }
-      ],
+      polls: [],
       voted: null
     };
   },
@@ -57,6 +48,23 @@ export default {
   updated() {
     console.log(this.voted);
     // console.log()
+  },
+  created() {
+    // fetch data from db
+    db.collection("polls")
+      .get()
+      .then(snapshot => {
+        const queryPolls = [];
+        snapshot.forEach(doc => {
+          const poll = doc.data();
+          poll.id = doc.id;
+          queryPolls.push(poll);
+        });
+        this.polls = queryPolls;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 };
 </script>
