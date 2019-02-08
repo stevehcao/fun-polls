a112916cf805fa8044d0c79cb3c7f34fc50d12ee
Steve Cao <steve.cao7@gmail.com>
2019-02-07 20:03:16 -0800

    structuring possible app and components

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <nav>possible nav/header here</nav>
+    <h1>Home Page</h1>
+    <Poll/>
+  </div>
+</template>
+
+<script>
+import Poll from "@/components/Poll";
+
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+  components: {
+    Poll
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/Poll.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="poll">
+    <h1>Poll goes here</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Poll",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
