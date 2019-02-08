cc96489ce83d96cdba43905e724590558c2bdcc3
Steve Cao <steve.cao7@gmail.com>
2019-02-08 01:05:19 -0800

    local voting mechanism working

--- src/components/Poll.vue
+++ src/components/Poll.vue
@@ -3,15 +3,16 @@
     <h1>Poll goes here</h1>
     <div class="question">
       <h3>{{ polls[0].question }}</h3>
-      <form>
+      <form @submit.prevent="countVote(voted)">
         <!-- radio buttons or checkbox for each question choice have to loop through the ans array -->
         <div v-for="(answer, idx) in polls[0].answers" :key="idx">
           <!-- value is what you are going to add once you check the box -->
           <!-- <input type="checkbox" :id="answer.id" :value="answer" v-model="voted">
           <label :for="answer.id">{{answer.choice}}</label>-->
-          <input type="radio" :id="answer.id" :value="answer" v-model="voted">
+          <input type="radio" :id="answer.id" :value="idx" v-model="voted">
           <label :for="answer.id">{{ answer.choice }}</label>
         </div>
+        <button>Rock the Vote!</button>
       </form>
     </div>
   </div>
@@ -38,6 +39,17 @@ export default {
       ],
       voted: null
     };
+  },
+  methods: {
+    countVote(idx) {
+      const choice = this.polls[0].answers[idx];
+      choice.votes++;
+      console.log(choice.votes, choice.choice, "VOTES");
+      // increase vote count by 1
+      // send updated poll question to db can grab one doc by using .doc(id)
+      // after async function runs, in the then() push router to stats page
+      // maybe send info somehow to stats page?
+    }
   },
   // checking check box form
   updated() {
