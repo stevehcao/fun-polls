feaaca11f7728b02ce0f3b88adfca54572c47c14
Steve Cao <steve.cao7@gmail.com>
2019-02-08 00:46:56 -0800

    added a radio button choice for poll, using dummy data

--- src/components/Poll.vue
+++ src/components/Poll.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="poll">
     <h1>Poll goes here</h1>
+    <div class="question">
+      <h3>{{ polls[0].question }}</h3>
+      <form>
+        <!-- radio buttons or checkbox for each question choice have to loop through the ans array -->
+        <div v-for="(answer, idx) in polls[0].answers" :key="idx">
+          <!-- value is what you are going to add once you check the box -->
+          <!-- <input type="checkbox" :id="answer.id" :value="answer" v-model="voted">
+          <label :for="answer.id">{{answer.choice}}</label>-->
+          <input type="radio" :id="answer.id" :value="answer" v-model="voted">
+          <label :for="answer.id">{{ answer.choice }}</label>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -8,7 +21,28 @@
 export default {
   name: "Poll",
   data() {
-    return {};
+    return {
+      // polls are an array of objects that contains poll questions and answers
+      // dummy data for now until we hook up database
+      // possibly grab all data and then shuffle using Fisher-Yates
+      polls: [
+        {
+          question: "What is your favorite 2018 video game?",
+          answers: [
+            { choice: "Spider-Man", votes: 0, id: 1 },
+            { choice: "Super Smash Bros. Ultimate", votes: 0, id: 2 },
+            { choice: "Red Dead Redemption 2", votes: 0, id: 3 },
+            { choice: "God of War", votes: 0, id: 4 }
+          ]
+        }
+      ],
+      voted: null
+    };
+  },
+  // checking check box form
+  updated() {
+    console.log(this.voted);
+    // console.log()
   }
 };
 </script>
