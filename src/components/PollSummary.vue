c9ea47b3a2f92273182a20d23f624c3dd3d4abe7
Steve Cao <steve.cao7@gmail.com>
2019-02-11 01:07:00 -0800

    poll summary css styling

--- src/components/PollSummary.vue
+++ src/components/PollSummary.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="poll-summary container">
-    <div v-for="(ans, idx) in ansArr" :key="idx">{{ans.question}}: {{ans.ans}}</div>
+    <h3 class="poll-summary-title orange-text lighten-1">What you voted on</h3>
+    <div class="flow-text" v-for="(ans, idx) in ansArr" :key="idx">
+      <span class="blue-text accent-2">{{ans.question}}</span>
+      <blockquote>{{ans.ans}}</blockquote>
+    </div>
   </div>
 </template>
 
